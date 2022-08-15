6889bde7e1c63cf113d8fcd02037e8c8a878bdd7
Vyacheslav Molodyakov <vrmolodyakov@mail.ru>
2022-08-15 03:15:50 +0300

    Init Test for Redis

--- internal/adapters/redis/redis_test.go
+++ internal/adapters/redis/redis_test.go
@@ -4,14 +4,35 @@ import (
 	"testing"
 
 	"github.com/alicebob/miniredis"
+	"github.com/go-redis/redis"
 )
 
-var redisServer *miniredis.Miniredis
+var (
+	redisServer *miniredis.Miniredis
+	redisClient *redis.Client
+)
 
-func TestSave(t *testing.T) {
+func TestUrlSave(t *testing.T) {
 	t.Log("start")
 }
 
 func setUp() {
+	redisServer = mockRedis()
+	redisClient = redis.NewClient(&redis.Options{
+		Addr: redisServer.Addr(),
+	})
+}
 
+func mockRedis() *miniredis.Miniredis {
+	s, err := miniredis.Run()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return s
+}
+
+func teardown() {
+	redisServer.Close()
 }
